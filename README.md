198ac3664051ea69cc87b9381cde2e17d7a55b07
MARCOS EDUARDO EDUARDO <122810400+eduislas84@users.noreply.github.com>
2023-01-24 20:45:14 +0000

    Programa3: Print y Operaciones Aritméticas

--- README.md
+++ README.md
@@ -6,3 +6,5 @@
 |--|--|--|
 |1.|programa0.py|Hola mundo en python|
 |2.|programa1.py|Comentarios multilinea, comentarios de una linea y concatenacion de una linea|
+|3.|programa2.py|Comentarios multilinea|
+|4.|programa3.py|Print y Operaciones Aritméticas|
---
+++ Semana2/Programa3.py
@@ -0,0 +1,12 @@
+"""
+     programa3
+      Nombre: Marcos Eduardo I. H.
+      Fecha: 24/01/2023 
+      Descripcion: Print y Operaciones Aritméticas
+"""
+numero1 = 10
+numero2 = 5
+print(numero1 + numero2) 
+print("{}+{}={}".format(numero1,numero2,(numero1 + numero2)))
+print("{}={}*{}".format((numero1*numero2),numero1,numero2))
+print("El resultado de dividir {} / {} es {}".format(numero1,numero2,(numero1 / numero2)))
