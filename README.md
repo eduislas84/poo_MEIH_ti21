b47ae2ffa002bb87fdf4924084c07114697723e6
MARCOS EDUARDO EDUARDO <122810400+eduislas84@users.noreply.github.com>
2023-01-24 19:40:27 +0000

    Programa 1 Se creo el archivo

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 ## indice
 
-|No|Archivo|Descripsión|
+|No|Archivo|Descripción|
 |--|--|--|
 |1.|programa0.py|Hola mundo en python|
+|2.|programa1.py|Comentarios multilinea, comentarios de una linea y concatenacion de una linea|
---
+++ Semana2/programa1.py
@@ -0,0 +1,9 @@
+"""
+     programa1
+      Nombre: Marcos Eduardo I. H.
+      Fecha: 23/01/2023 
+      Descripcion: Comentarios multilinea, comentarios de una linea y concatenacion de una linea
+"""
+variable1 = "Hola" # Variable que almacena una cadena de caracter
+variable2 = '619'  # Variable que almacena una cadena de caracteres
+print(variable1 + variable2) # Conecta dos cadenas
