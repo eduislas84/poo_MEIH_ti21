2433ad9913c5e6d7a356efc11c333ea93c2db054
MARCOS EDUARDO EDUARDO <122810400+eduislas84@users.noreply.github.com>
2023-01-24 19:57:41 +0000

    Programa2: Comentarios Multimedia

---
+++ Semana2/Programa2.py
@@ -0,0 +1,9 @@
+"""
+     programa2
+      Nombre: Marcos Eduardo I. H.
+      Fecha: 24/01/2023 
+      Descripcion: Comentarios Multimedia
+"""
+variable1 = "Hola" 
+variable2 = ' 619'
+print("{}{}".format(variable1,variable2)) 
