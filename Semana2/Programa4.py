b6b33ccdc217408c54c1949f37883320d57801f0
MARCOS EDUARDO EDUARDO <122810400+eduislas84@users.noreply.github.com>
2023-01-26 20:35:22 +0000

    Programa 4

---
+++ Semana2/Programa4.py
@@ -0,0 +1,12 @@
+"""   
+      programa2
+      Nombre: Marcos Eduardo I. H.
+      Fecha: 24/01/2023 
+      Descripcion: A
+"""
+numero1 = 10
+numero2 = 5
+print("{} + {} = {} ".format(numero1 ,numero2, numero1 + numero2))
+print(" {n1} + {n2} = {suma} ".format(n1=numero1, n2=numero2, suma=numero1 + numero2))
+print(" {n2} + {n2} = {n2} ".format(n1=numero1, n2=numero2, suma=numero1 + numero2))
+print(" {numero1} + {numero2} = {suma} ".format(numero1=numero1, numero2=numero2, suma= numero1 + numero2))
--- semana1/programa0.py
+++ semana1/programa0.py
@@ -1,1 +1,2 @@
-*print (Hola mundo en Phyton) # Imprime mensaje de consola
+print ("Hola mundo en Phyton") # Imprime mensaje de consola
+print ("Editado desde Replit") # Imprime mensaje de consola
