f0e62f16a6ac49510de9dd1879be24962aa3ba79
MARCOS EDUARDO EDUARDO <122810400+eduislas84@users.noreply.github.com>
2023-01-30 16:59:46 +0000

    sasdfsdfsdf

---
+++ Semana3/Programa5.py
@@ -0,0 +1,26 @@
+"""   
+      programa2
+      Nombre: Marcos Eduardo I. H.
+      Fecha: 30/01/2023 
+      Descripcion: Resta multi
+"""
+numero1 = input("Numero 1:")
+numero2 = 10
+suma = int(numero1) + numero2
+print(suma)
+numero1 = input("Numero 1:")
+numero2 = 10
+resta = int(numero1) - numero2
+print(resta)
+numero1 = input("Numero 1:")
+numero2 = 10
+multiplicacion = int(numero1) * numero2
+print(multiplicacion)
+numero1 = input("Numero 1:")
+numero2 = 10
+división = int(numero1) / numero2
+print(división)
+numero1 = input("Numero 1:")
+numero2 = 10
+potencia = int(numero1) ** numero2
+print(potencia)
